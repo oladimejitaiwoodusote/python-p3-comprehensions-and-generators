c83a9bf21f70fb8f2a4c0ad63aa4221b4d4a71d6
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-04-02 09:35:35 -0400

    python lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [i for i in num_list if i % 2 == 0]
+    return even_list
+    
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [(i + "!") for i in sentence_list]
+    return exclamation_list
+
+
+print(return_evens([0,1]))
+
+print (make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
